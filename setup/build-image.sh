87a4c63bb84b8aa5677634082c7d47a71337cfd5
vr100 <vr100@users.noreply.github.com>
2020-12-22 00:59:15 +0530

    adding octave docker

--- setup/build-image.sh
+++ setup/build-image.sh
@@ -9,6 +9,8 @@ elif [ $1 == "nodejs" ]; then
 	docker build --tag nodejs -f nodejs-docker .
 elif [ $1 == "qiskit" ]; then
 	docker build --tag qiskit -f qiskit-docker .
+elif [ $1 == "ml-learn" ]; then
+	docker build --tag ml-learn -f octave-docker .
 else
 	echo "Image name: $1 not supported"
 fi
